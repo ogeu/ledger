47a5c5f18c6eedc48202f24ba332a0edef0c2c49
Takahiro Kosaka <takahiro@snow.local>
2017-03-08 01:20:45 +0000

    Adds accounts table. Inserts some rows as an example.

---
+++ src/main/db/data.sql
@@ -0,0 +1,5 @@
+insert into currencies( id, scale ) values ( 'JPY', 0 );
+insert into currencies( id, scale ) values ( 'USD', 2 );
+insert into accounts( id, name, currency, sortkey ) values ( '3OcJiVVkFedf8dzb', 'Cash', 'JPY', 0 );
+insert into accounts( id, name, currency, sortkey ) values ( 'PBPJNKS7PdoOIBUD', 'Cash', 'USD', 1 );
+insert into accounts( id, name, currency, sortkey ) values ( 'BukO5f2ZHW9TPe7r', 'Wells Fargo', 'USD', 2 );
--- src/main/db/objects.sql
+++ src/main/db/objects.sql
@@ -14,3 +14,14 @@ create table currencies
    id char( 3 ) primary key, -- ISO 4217 currency code
    scale smallint not null check ( scale >= 0 and scale <= 8 ) -- e.g., 2 for USD, 0 for JPY, 8 for BTC
 );
+
+
+
+create table accounts
+(
+   id char( 16 ) primary key,
+   name text not null,
+   currency char( 3 ) not null,
+   sortKey smallint not null unique,
+   foreign key ( currency ) references currencies( id ) on delete restrict on update cascade
+);
